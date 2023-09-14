b582c392e7d2318e5f7517d477f94bd2e4f6d125
Zardron <zardron@clockworkdelivery.com>
2023-09-14 16:38:47 +0400

    using deployed backend

--- backend/server.js
+++ backend/server.js
@@ -16,12 +16,19 @@ const roomRouter = require("./routes/roomRoutes");
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(cors());
+const corsOptions = {
+  origin: "http://localhost:3000",
+  credentials: true, //access-control-allow-credentials:true
+  optionSuccessStatus: 200,
+};
+app.use(cors(corsOptions));
 
 // Routes
 app.use("/api/user", authRouter);
 app.use("/api/room", roomRouter);
-
+app.get("/", (req, res) => {
+  res.send("Hey this is my API running 🥳");
+});
 // Middleware
 // Middlewares
 app.use(notFound);
